8871ddb39cd32e1028512cd136fc31c2d33293ba
GAJENDRAKUMARPRAJAPATI <108794397+GAJENDRAKUMARPRAJAPATI@users.noreply.github.com>
2023-04-18 12:12:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import SearchItem from './component/SearchItem';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchItem/>
+    </div>
+  );
+}
+
+export default App;
